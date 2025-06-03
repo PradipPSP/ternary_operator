78017e0c86b61b71347a8606a6da1a7ee8fc2d8e
Pradip Mondal <pradipbd.professional@gmail.com>
2025-06-04 00:52:51 +0600

    solve problem 1

--- ternary.js
+++ ternary.js
@@ -1,1 +1,9 @@
 
+// Ternary Operators
+// syntax: condition ? value_if_true : value_if_false;
+
+// Problem_1: Find Even or Odd numbers?
+
+let num = 2;
+let result = num % 2 === 0 ? "Even Number" : "Odd Number";
+console.log(result); 
