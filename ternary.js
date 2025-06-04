6046592cae90fd07e5c5c9152b7290448cf9cea6
Pradip Mondal <pradipbd.professional@gmail.com>
2025-06-04 16:35:14 +0600

    ternary operators

--- ternary.js
+++ ternary.js
@@ -7,3 +7,19 @@
 let num = 2;
 let result = num % 2 === 0 ? "Even Number" : "Odd Number";
 console.log(result); 
+
+
+// find Maximum?
+
+let a =40;
+let b = 30;
+let maxNum = a > b ? 'A is a Maximum Number' : 'B is a Maximum Number';
+console.log(maxNum);
+
+
+// Pass of Fail?
+
+let score = '60';
+let passFail = score >= 50 ? 'Pass' : score >= '50' ? 'Pass' :'Fail';
+console.log(passFail);
+
